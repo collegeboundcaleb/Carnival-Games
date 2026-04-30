78bb8d345e5e4340f08120aed71c00e68495f565
DaBlaze <7017951+DaBlaze@users.noreply.github.com>
2026-04-30 13:19:39 -0500

    Update targetLogic.cs

--- Carnival Games/Assets/Scripts/targetLogic.cs
+++ Carnival Games/Assets/Scripts/targetLogic.cs
@@ -2,25 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class targetLogic : MonoBehaviour
+public class TargetLogic : MonoBehaviour
 {
+    private GameObject headset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        headset = GameObject.Find("CenterEyeAnchor");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (headset != null)
+        {
+            LookAtPlayer();
+        }
+        else
+        {
+            headset = GameObject.Find("CenterEyeAnchor");
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void LookAtPlayer()
     {
-        if (other.CompareTag("Projectile"))
+        // Look toward player
+        Vector3 direction = headset.transform.position - transform.position;
+        direction.y = 0f;
+
+        if (direction != Vector3.zero)
         {
-            Destroy(gameObject);
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+            transform.rotation = Quaternion.Euler(
+                0f,
+                targetRotation.eulerAngles.y,
+                0f
+            );
         }
     }
 }
